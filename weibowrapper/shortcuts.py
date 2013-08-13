e2217be37c34c787bc348e2881cd6dc8842647fe
sheep9275 <sheep9275@gmail.com>
2013-08-14 00:40:26 +0800

    Refined update and search shortcuts

--- weibowrapper/shortcuts.py
+++ weibowrapper/shortcuts.py
@@ -102,10 +102,11 @@ def get_user_feed(account, uid):
     return result
 
 #=====================================================================================
-# Local Image
+# Download All Shortcuts
 #=====================================================================================
 
 def download_all_follower (account):
+    print('Begin downloading follower')
     query = {'count': 200}
     result = account.call_api(conf.API_FOLLOWER, query)
     follower_list = result['users']
@@ -113,12 +114,12 @@ def download_all_follower (account):
         query['cursor'] = result['next_cursor']
         result = account.call_api(conf.API_FOLLOWER, query)
         follower_list += result['users']
-    for profile in follower_list:
-        os.system('wget -O ' + conf.PATH_AVATAR + '/' + profile['idstr'] + '.jpg ' + profile['avatar_large'])
     with open(conf.PATH_FOLLOWER_JSON, 'w') as f:
         f.write(json.dumps(follower_list))
-            
+    print('Finished downloading follower')
+
 def download_all_following (account):
+    print('Begin downloading following')
     query = {'count': 200}
     result = account.call_api(conf.API_FOLLOWING, query)
     following_list = result['users']
@@ -126,18 +127,20 @@ def download_all_following (account):
         query['cursor'] = result['next_cursor']
         result = account.call_api(conf.API_FOLLOWING, query)
         following_list += result['users']
-    for profile in following_list:
-        os.system('wget -O ' + conf.PATH_AVATAR + '/' + profile['idstr'] + '.jpg ' + profile['avatar_large'])
     with open(conf.PATH_FOLLOWING_JSON, 'w') as f:
         f.write(json.dumps(following_list))
+    print('Finished downloading following')
 
 def download_all_myfeed(account):
+    print('Begin downloading my feed')
     result = account.call_api(conf.API_MYFEED, {'count': 100})
     myfeed_list = result['statuses']
     with open(conf.PATH_MYFEED_JSON, 'w') as f:
         f.write(json.dumps(myfeed_list))
+    print('Finished downloading my feed')
 
 def download_all_archive(account):
+    print('Begin downloading archive')
     query = {'count':50, 'page':1}
     first_round = True
     total = 1
@@ -154,8 +157,10 @@ def download_all_archive(account):
         query['page'] = query['page'] + 1
     with open(conf.PATH_ARCHIVE_JSON, 'w') as f:
         f.write(json.dumps(feed_list))
+    print('Finished downloading archive')
 
 def download_all_timeline(acocunt):
+    print('Begin downloading timeline')
     in_progress = True
     first_round = True
     while (in_progress):
@@ -172,29 +177,11 @@ def download_all_timeline(acocunt):
                 f.write(json.dumps(tweet))
         if result['next_cursor'] == 0:
             in_progress = False
-
+    print('Finished downloading timeline')
 
 #=====================================================================================
 # Search Functionality
 #=====================================================================================
-
-def db_pull_timeline(acocunt):
-    in_progress = True
-    first_round = True
-    while (in_progress):
-        if first_round:
-            result = acocunt.call_api(conf.API_TIMELINE, {'count':100})
-            first_round = False
-        else:
-            result = acocunt.call_api(conf.API_TIMELINE, {'count': 100, 'max_id': result['next_cursor']})
-        for tweet in result['statuses']:
-            path = conf.PATH_FEED_DB + '/' + str(tweet['user']['id'])
-            if not os.path.exists(path):
-                os.mkdir(path)
-            with open(path+'/'+str(tweet['id']), 'w') as f:
-                f.write(json.dumps(tweet))
-        if result['next_cursor'] == 0:
-            in_progress = False
 
 def index_db():
     schema = Schema( path     = TEXT(stored=True),
@@ -306,3 +293,29 @@ def search_my_archive(query_str):
         
 def search_all(query_str):
     return search_db(query_str) + search_myfeed(query_str) + search_my_archive(query_str)
+
+def search_weibo(query_str, domain='all-feed'):
+    if domain == 'all-feed':
+        return search_all(query_str)
+    elif domain == 'my-feed':
+        return search_myfeed(query_str)
+    elif domain == 'home-timeline':
+        return search_db(query_str)
+    elif domain == 'archive':
+        return search_my_archive(query_str)
+    else:
+        return None
+
+#=====================================================================================
+# Adminidtration Shortcuts
+#=====================================================================================
+
+def update_all_db(account):
+    download_all_follower(account)
+    download_all_following(account)
+    download_all_myfeed(account)
+    download_all_timeline(account)
+    download_all_archive(account)
+    index_db()
+    index_myfeed()
+    index_archive()
